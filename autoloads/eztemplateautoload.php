11ca03aec7537c361ccad8c2d55a30cc83f35dd7
David Sayre <david.sayre@beaconfire.com>
2012-09-10 15:47:15 -0400

    bfsurveycaptcha INIT

---
+++ autoloads/eztemplateautoload.php
@@ -0,0 +1,8 @@
+<?php
+$eZTemplateOperatorArray = array();
+ 
+$eZTemplateOperatorArray[] = array( 'script' => 'extension/bfsurveycaptcha/autoloads/bfsurveyoperators.php',
+                                   'class' => 'BFSurveyOperators',
+                                   'operator_names' => array( 'validatecaptcha' )
+);
+?>
---
+++ settings/ezsurvey.ini.append.php
@@ -0,0 +1,10 @@
+<?php /* #?ini charset="utf-8"?
+ 
+[QuestionTypeSettings]
+ExtensionDirectories[]=bfsurveycaptcha
+ 
+[QuestionTypeSettings_bfsurveycaptcha]
+QuestionTypeList[]
+QuestionTypeList[]=captcha
+ 
+*/ ?>
